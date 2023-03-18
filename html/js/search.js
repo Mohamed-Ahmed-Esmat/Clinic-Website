b771e80f896e45a48de31aec2e77e286fcf4fe07
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-03-18 23:02:43 +0200

    Testing\

---
+++ html/js/search.js
@@ -0,0 +1,9 @@
+function showSearch() {
+    var searchBar = document.getElementById("searchBar");
+    if (searchBar.style.display === "none") {
+      searchBar.style.display = "block";
+    } else {
+      searchBar.style.display = "none";
+    }
+  }
+  
