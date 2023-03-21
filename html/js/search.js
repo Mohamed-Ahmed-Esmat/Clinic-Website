c5e4be393057a30f35a0864e5e1a28a28afb3455
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-03-21 07:51:59 +0200

    deleted file

--- html/js/search.js
+++
@@ -1,9 +0,0 @@
-function showSearch() {
-    var searchBar = document.getElementById("searchBar");
-    if (searchBar.style.display === "none") {
-      searchBar.style.display = "block";
-    } else {
-      searchBar.style.display = "none";
-    }
-  }
-  
